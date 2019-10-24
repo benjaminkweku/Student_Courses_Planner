3f4e4dba2d980dc9ed6bc3de87a5f9a2e7d570c7
Fourteen98 <emuhiydeen6@gmail.com>
2019-10-24 17:57:50 +0000

    Merge branch 'master' of https://github.com/Fourteen98/Student_Courses_Planner

--- app/src/main/java/my/benjamin/com/nar_drawer/RegisterCourse.java
+++ app/src/main/java/my/benjamin/com/nar_drawer/RegisterCourse.java
@@ -9,12 +9,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import my.benjamin.com.nar_drawer.ui.DatabaseManager;
+
 public class RegisterCourse extends AppCompatActivity {
     //setting button variable
     private TextView program;
     private EditText edit_input;
     private Button add;
     static boolean check = true;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +28,8 @@ public class RegisterCourse extends AppCompatActivity {
         program=(TextView)findViewById(R.id.downbar);
         edit_input=(EditText)findViewById(R.id.edt_save);
         add=(Button)findViewById(R.id.add);
-
+        DatabaseManager db = new DatabaseManager(this);  // an object for the DatabaseManager
+        // Class, parameter this obj to context obj;
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,8 +39,10 @@ public class RegisterCourse extends AppCompatActivity {
                     if (program.getText() != edit_input.getText()) {
                         program.setText(edit_input.getText().toString());
                         check = false;
+
                     }
                 }
+
             }
         });
     }
